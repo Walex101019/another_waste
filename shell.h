8e4d55c0e296c708399b44fbca4cd32e98c23780
Walex101019 <ibietanwasiu94@gmail.com>
2023-10-18 18:06:45 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_LENGTH 100
+#define MAX_ARG_COUNT 10
+
+int parse_command(char *input, char *args[]);
+int execute_command(char *args[]);
+void print_environment();
+void display_prompt();
+
+
+
+extern char **environ;
+
+#endif /* SHEL.H */
